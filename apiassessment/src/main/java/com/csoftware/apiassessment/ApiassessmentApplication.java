60bccadd323f7f18fbc939d1d4bc3cc80f466e3e
veron <fveron-07@hotmail.com>
2020-01-15 19:52:07 -0500

    First one as commit

---
+++ apiassessment/src/main/java/com/csoftware/apiassessment/ApiassessmentApplication.java
@@ -0,0 +1,13 @@
+package com.csoftware.apiassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiassessmentApplication.class, args);
+	}
+
+}
---
+++ apiassessment/src/test/java/com/csoftware/apiassessment/ApiassessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csoftware.apiassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiassessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
